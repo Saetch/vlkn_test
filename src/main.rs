3fe74e5b3d6af6c2bf3ef70292576cfca92fcc21
Saetch <nils.wriedt@hotmail.com>
2022-05-10 00:08:15 +0200

    removed performance test

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,5 @@ use std::time::{Duration, SystemTime};
 fn main() {
     let instance = Instance::new(InstanceCreateInfo::default()).expect("failed to create instance");
     let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
-    let mut x:i64 = 0;
-    let mut rng = rand::thread_rng();
-    let now = SystemTime::now();
-    for i in 0 as i64 ..100000000  {
-        x+= rng.gen::<i32>() as i64 % 5 ;
-    }
 
-    println!("Interesting! {}, this took {} ms", &x, now.elapsed().unwrap().as_millis());
-    println!("Hey, I've finished successfully!" );
 }
