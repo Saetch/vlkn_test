c547c9bd74aa64193901f286d573412dccd372fc
Saetch <nils.wriedt@hotmail.com>
2022-05-10 01:21:36 +0200

    added initialization steps all the way up to creating a buffer

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,71 @@
 use rand::Rng;
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::device::physical::PhysicalDevice;
+use std::sync::Arc;
 use std::time::{Duration, SystemTime};
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::device::{Device, Features, DeviceCreateInfo, QueueCreateInfo, Queue};
+use bytemuck::{Pod, Zeroable};
+
 fn main() {
+    //initialize vulkan and get the physical device (GPU)
     let instance = Instance::new(InstanceCreateInfo::default()).expect("failed to create instance");
     let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+
+    //check the selected physical device for queue families. These are groupings of queues, which are kind of like threads on the GPU
+    //every queue family is responsible for specific actions (overlap possible)
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+    
+    //get all queue families from the physical device and chose the one that supports graphics processing
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+    .expect("couldn't find a graphical queue family");
+
+
+    //create a new device (communication channel to physical device) <- left
+    //for the chosen GPU
+    //and queues based on the device -> right
+    let (device, mut queues) = Device::new(
+        physical,
+        DeviceCreateInfo {
+            // here we pass the desired queue families that we want to use
+            queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+    
+
+    //chose the graphics queue. In this case, it is expected that we only got 1 queue, even tho multiple queues are theoretically possible
+    let v_q :Vec<Arc<Queue>> = queues.collect();
+    if v_q.len() > 1 {
+        println!("Something went wrong, queues result unexpected!");
+    }
+
+    let queue = v_q.first().unwrap();
+
+// here we derive all these traits to ensure the data behaves as simple as possible
+#[repr(C)]      //organize the struct in memory as if it was a C struct
+#[derive(Default, Copy, Clone, Zeroable, Pod)]      //derive Traits Default value, Copy, Clone, init with all 0 Bytes and plain old data (no functions or fancies) for the struct
+struct MyStruct {
+    a: u32,
+    b: u32, 
+}                   //only types that implement Send and Sync or are static can be used fully in a buffer context
+
+
+
+    let data = MyStruct { a: 5, b: 69 };
+    let buffer = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), false, data)
+    .expect("failed to create buffer"); //expect will unpack a Result (or Option) and throw an unrecoverable, willing Error with the given message on fail
+
+
+
+ /*    let iter = (0..128).map(|_| 5u8);                
+    let buffer =                        
+    CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, iter).unwrap();  //create a buffer with 128 elements containing the number 5 in u8
+    */            
+
 
 }
