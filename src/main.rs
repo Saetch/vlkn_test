9b49feb518a9ee97b853fe9e2683603715ddb6ac
Saetch <nils.wriedt@hotmail.com>
2022-05-12 19:22:13 +0200

    finalized drawing the Mandelbrot set to a .png

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use vulkano::pipeline::PipelineBindPoint;
 use vulkano::image::{ImageDimensions, StorageImage};
 use vulkano::format::{Format, ClearValue};
 use image::{ImageBuffer, Rgba};
+use vulkano::image::view::ImageView;
 
 fn main() {
     //initialize vulkan and get the physical device (GPU)
@@ -275,4 +276,130 @@ struct MyStruct {
     image.save("image.png").unwrap();
 
     println!("Everything succeeded!");
+
+
+
+    mod mandelbrot_shader {
+
+        vulkano_shaders::shader!{
+            ty: "compute",                  //calculate for a given pixel the mandelbrot set. The closer the pixel is to being in the set, the brighter it will be. In the set = complete white
+            src: "
+            #version 450
+
+            layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+            
+            layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
+            
+            void main() {
+                vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
+                vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+            
+                vec2 z = vec2(0.0, 0.0);
+                float i;
+                for (i = 0.0; i < 1.0; i += 0.005) {
+                    z = vec2(
+                        z.x * z.x - z.y * z.y + c.x,
+                        z.y * z.x + z.x * z.y + c.y
+                    );
+            
+                    if (length(z) > 4.0) {
+                        break;
+                    }
+                }
+            
+                vec4 to_write = vec4(vec3(i), 1.0);
+                imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
+            }
+            "
+        }
+    }
+
+    //create a new image to draw to
+    let image = StorageImage::new(
+        device.clone(),
+        ImageDimensions::Dim2d {
+            width: 1024,
+            height: 1024,
+            array_layers: 1,
+        },
+        Format::R8G8B8A8_UNORM,
+        Some(queue.family()),
+    )
+    .unwrap();
+    
+    //create an ImageView, which is needed in order to pass the image to the GPU shader
+    let view = ImageView::new_default(image.clone()).unwrap();
+
+
+    let shader = mandelbrot_shader::load(device.clone()).expect("failed to create ShaderModule for Mandelbrot");
+        //compute pipeline to actually execute the shader
+        let compute_pipeline = ComputePipeline::new(
+            device.clone(),
+            shader.entry_point("main").unwrap(),
+            &(),
+            None,
+            |_| {},
+        )
+        .expect("failed to create compute pipeline");
+
+
+    //create a descriptor set that includes the ImageView we just created
+    let layout = compute_pipeline.layout().set_layouts().get(0).unwrap();
+    let set = PersistentDescriptorSet::new(
+        layout.clone(),
+        [WriteDescriptorSet::image_view(0, view.clone())], // 0 is the binding
+    )
+    .unwrap();
+
+    //create a buffer to store the output in
+    let buf = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        false,
+        (0..1024 * 1024 * 4).map(|_| 0u8),
+    )
+    .expect("failed to create buffer");
+
+
+    //create a CommandBufferBuilder for the actual commandBuffer
+    let mut builder = AutoCommandBufferBuilder::primary(
+        device.clone(),
+        queue.family(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .unwrap();
+
+    //create a CommandBuffer with the correct commands to submit
+    builder
+        .bind_pipeline_compute(compute_pipeline.clone())
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            0,
+            set,
+        )
+        .dispatch([1024 / 8, 1024 / 8, 1])
+        .unwrap()
+        .copy_image_to_buffer(image.clone(), buf.clone())
+        .unwrap();
+    
+    let command_buffer = builder.build().unwrap();
+    
+
+    //submit the commands to the GPU
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    //wait for the execution to be finished
+    future.wait(None).unwrap();
+
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("Mandelbrot.png").unwrap();
+
+    println!("Everything succeeded!");
+
 }
