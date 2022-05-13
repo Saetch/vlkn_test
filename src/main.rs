f4b92d71181480142cfa2f8a5d83cbe486986ec8
Saetch <nils.wriedt@hotmail.com>
2022-05-13 14:57:20 +0200

    added function to check for GPU swapchain compatibility

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 
 use rand::Rng;
 use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::device::physical::PhysicalDevice;
+use vulkano::device::physical::{PhysicalDevice, PhysicalDeviceType, QueueFamily};
+use vulkano::swapchain::Surface;
 use vulkano_win::VkSurfaceBuild;
-use winit::event_loop::EventLoop;
-use winit::window::WindowBuilder;
+use winit::event_loop::{EventLoop, ControlFlow};
+use winit::window::{WindowBuilder, Window};
 use std::process::CommandArgs;
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
@@ -21,6 +22,7 @@ use vulkano::image::{ImageDimensions, StorageImage};
 use vulkano::format::{Format, ClearValue};
 use image::{ImageBuffer, Rgba};
 use vulkano::image::view::ImageView;
+use vulkano::device::DeviceExtensions;
 
 use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
 use vulkano::pipeline::graphics::vertex_input::BuffersDefinition;
@@ -28,10 +30,44 @@ use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
 use vulkano::pipeline::GraphicsPipeline;
 use vulkano::render_pass::{Subpass, Framebuffer, FramebufferCreateInfo};
 use winit::event::{Event, WindowEvent};
-use winit::event_loop::ControlFlow;
+
+
+fn select_physical_device<'a>(
+    instance: &'a Arc<Instance>,
+    surface: Arc<Surface<Window>>,
+    device_extensions: &DeviceExtensions,
+) -> (PhysicalDevice<'a>, QueueFamily<'a>) {
+    let (physical_device, queue_family) = PhysicalDevice::enumerate(&instance)
+        .filter(|&p| p.supported_extensions().is_superset_of(&device_extensions))
+        .filter_map(|p| {
+            p.queue_families()
+                .find(|&q| q.supports_graphics() && q.supports_surface(&surface).unwrap_or(false))
+                .map(|q| (p, q))
+        })
+        .min_by_key(|(p, _)| match p.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            PhysicalDeviceType::Other => 4,
+        })
+        .expect("no device available");
+
+    (physical_device, queue_family)
+}
+
 fn main() {
     //initialize vulkan and get the physical device (GPU)
     let instance = Instance::new(InstanceCreateInfo::default()).expect("failed to create instance");
+
+
+    //only needed when using swapchains, e.g. rendering to the viewport
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::none()
+    };
+
+
     let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
 
     //check the selected physical device for queue families. These are groupings of queues, which are kind of like threads on the GPU
@@ -632,7 +668,7 @@ struct MyStruct {
 
     //tell vulkan that we need to load extensions in order to render to the viewport
     let required_extensions = vulkano_win::required_extensions();
-    //this is required, too. But I have no idea why. Something about the need to forward the instance to the window
+    //this is required, too. But I have no idea why. Something about the need to forward the instance to the window. This essentially loads a different configuration for vulkan
     let instance = Instance::new(InstanceCreateInfo {
         enabled_extensions: required_extensions,
         ..Default::default()
